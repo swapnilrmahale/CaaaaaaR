77a2a4b66aa350b923c2662306d454838d8e164d
swapnilrmahale <swapnilrmahale@gmail.com>
2015-07-29 20:05:39 +0530

    Created Web Project

---
+++ viame-web/target/viame-web/js/home.js
@@ -0,0 +1,82 @@
+function onLoadCallback() {
+	console.log('onLoadCallback');
+}
+
+function onSignIn() {
+	console.log('onSignIn');
+
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log("Name: " + profile.getName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+}
+
+var slider;
+function carSelector() {
+	console.log(this.value);
+	location.href='#input-wrapper';
+	//slider.$Pause();
+}
+
+function abc(slideIndex, fromIndex)
+{
+	if (slideIndex == 0){
+		$(".select-car").each(function(){
+			$(this).on("click",carSelector);
+		});
+	}
+}
+
+$(function(){
+
+	var slider_container_options = { 
+		$AutoPlay: true,
+		//$FillMode: 2,
+		$DragOrientation: 1,
+		$AutoPlayInterval: 4000,
+		$PauseOnHover: 0,
+		$ArrowKeyNavigation: true,
+		$SlideDuration: 500,
+		$ArrowNavigatorOptions: {
+			$Class: $JssorArrowNavigator$,
+			$ChanceToShow: 2
+		}
+	};
+
+	slider = new $JssorSlider$('slider-container', slider_container_options);
+
+	slider.$On($JssorSlider$.$EVT_PARK, abc);
+
+	$(".dropdown-menu li a").click(function(){
+		$("#city-selector").text($(this).text());
+	});
+
+	var datetimepickerOption = {
+		format: 'ddd DD-MM-YYYY HH:mm',
+		viewMode: 'months',
+		stepping: 15
+	};
+
+	$('.multi-dropdown').multiselect({
+		buttonWidth: '200',
+		enableFiltering: true
+	});
+
+	$('#date-from').datetimepicker(datetimepickerOption);
+	$('#date-from').data("DateTimePicker").minDate(moment().add(1,'d').format(datetimepickerOption.format));
+
+	$('#date-to').datetimepicker(datetimepickerOption);
+	$('#date-to').data("DateTimePicker").disable()
+
+	$('#date-from').on("dp.change", function(e) {
+		console.log('setting min date for to picker');
+		$('#date-to').data("DateTimePicker").minDate(e.date);
+		$('#date-to').data("DateTimePicker").enable();
+	});
+});
